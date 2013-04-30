a1c74ca7e2b2487bb490efef2b3972be1608f97e
Luiz Fonseca <runeroniek@gmail.com>
2013-04-30 15:54:47 -0300

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pledgebank::Application.config.secret_key_base = '21b0d612cb1a6bed6c9f6742460c493875d2aa40506fbe6a288d805a4ce56b46ec647c721954dae7e904354beb3911008030a98e8d261aef6dc6effb4506d8f7'
